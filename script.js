6bee2f9b0fcfff5766e24c67ec726b7d98bc4dca
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-24 16:54:43 +0200

    created an event listener and targeted the input value

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const taskList = document.querySelector("ul.collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
+
+loadEventListeners();
+
+function loadEventListeners() {
+  //add task event
+  form.addEventListener("submit", addTask);
+}
+function addTask(e) {
+  e.preventDefault();
+  let values = taskInput.value;
+  console.log(values);
+}
