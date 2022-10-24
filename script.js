878e204e4adbb059033adb9047d72e5234cc9ba9
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-24 18:22:47 +0200

    parsed the tasks into an array in the local storage

--- script.js
+++ script.js
@@ -23,10 +23,30 @@ function addTask(e) {
   li.appendChild(link);
   taskList.appendChild(li);
 
-  taskInput.value =
-    ""; /*here i am clearing the input after submitting so it is ready for a second input to be submitted*/
+  /*here i am clearing the input after submitting so it is ready for a second input to be submitted*/
 
   link.addEventListener("click", function (e) {
     li.remove();
   });
+
+  let task = taskInput.value;
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  taskInput.value = "";
 }
+let tasks = JSON.parse(localStorage.getItem("tasks"));
+tasks.forEach((task) => {
+  console.log(task);
+
+  clearBtn.addEventListener("click", clearAll);
+  function clearAll(e) {
+    e.preventDefault();
+    let tasks = JSON.parse(localStorage.getItem("tasks")).clear();
+  }
+});
