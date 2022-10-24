1e04df478c993ba1c60b36e2dc39ec66d80b66bf
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-24 16:44:36 +0200

    created UI variables needed

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("task-form");
+const taskList = document.querySelector("ul.collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
