89a4106bde6211315ba17b1994f8f5fe18f5cb51
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-25 20:43:55 +0200

    persisted local storage data in the DOM

--- script.js
+++ script.js
@@ -8,11 +8,43 @@ loadEventListeners();
 
 function loadEventListeners() {
   //this function is for all the event listeners i want
-
   form.addEventListener("submit", addTask);
   clearBtn.addEventListener("click", clearTasks);
   filter.addEventListener("keyup", filterTasks);
+  //DOM Load event
+  document.addEventListener("DOMContentLoaded", getTasks);
 }
+
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.classList.add("collection-item");
+    li.appendChild(document.createTextNode(task));
+
+    const link = document.createElement("a");
+    link.className =
+      "delete-item secondary-content"; /*secondary-content is a class name from materialize*/
+    link.innerHTML = `<i class="fa fa-remove"></i>`;
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    /*here i am clearing the input after submitting so it is ready for a second input to be submitted*/
+
+    link.addEventListener("click", function (e) {
+      if (confirm("Are You Sure?")) {
+        li.remove();
+      }
+    });
+  });
+}
+
 function addTask(e) {
   e.preventDefault();
   const li = document.createElement("li");
@@ -43,6 +75,11 @@ function addTask(e) {
   }
   tasks.push(task);
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  tasks.forEach((task) => {
+    console.log(task);
+  });
+
   taskInput.value = "";
 }
 
@@ -59,20 +96,22 @@ function filterTasks(e) {
   /*i am able to loop through the lis here because querySelectorAll returns a node list, but if it was getElementByClass
   then we would have to convert it into an array to be able to loop through it*/
   /*i put it either as let something = e.target.value or something = input.value*/
-  /*indexOf being -1 always means that the item is not available*/
+  /*indexOf being -1 always means that the item is not available so being !=-1 or >-1 then it is available*/
   let values = e.target.value;
-  document.querySelectorAll(".collection-item").forEach((task) => {
-    let firstItem = task.firstChild.textContent;
-    if (firstItem.indexOf(values) != -1) {
-      task.style.display = "block";
+  let lis = document.querySelectorAll(".collection-item");
+  lis.forEach((li) => {
+    let liContent = li.textContent;
+    if (liContent.indexOf(values) != -1) {
+      li.style.display = "block";
     } else {
-      task.style.display = "none";
+      li.style.display = "none";
     }
   });
 }
 /* in the filterTasks :
 select the input value
 select all the lis
-loop through the lis
-put the first li and its textContent in a variable
-see if the first li whose index represents the input isnt -1 then display that li, otherwise dont*/
+loop through the lis and put their text content in a variable
+then we overlap the li text content with the value using liContent.indexOf(value)
+then since it is indexOf we can see if there is a match by going character by character, if it is -1 then the characters
+of the value and li textContent dont match*/
